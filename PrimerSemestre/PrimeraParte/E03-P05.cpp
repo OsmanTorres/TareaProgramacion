605d490350a88eefd74eba272779beb60bbfe502
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-19 16:19:57 -0500

    Update E03-P05.cpp

--- PrimerSemestre/PrimeraParte/E03-P05.cpp
+++ PrimerSemestre/PrimeraParte/E03-P05.cpp
@@ -21,13 +21,13 @@ int main() {
 	printf("Introdusca el Primer Valor    : "); scanf("%lf", &x);
 	printf("Introdusca el Segundo Valor : "); scanf("%lf", &y);
 	r = x + y;
-	printf("La suma de %.1f m%cs %.1f es: %.1f", x, 160, y, r);
+	printf("\n La suma de %.1f m%cs %.1f es :  %.1f", x, 160, y, r);
 	r = x - y;
-	printf("\nLa resta de %.1f menos %.1f es: %.1f", x, y, r);
+	printf("\n La resta de %.1f menos %.1f es :  %.1f", x, y, r);
 	r = x * y;
-	printf("\nLa multiplicaci%cn de %.1f por %.1f es: %.1f", 162, x, y, r);
+	printf("\n La multiplicaci%cn de %.1f por %.1f es :  %.1f", 162, x, y, r);
 	r = x / y;
-	printf("\nLa divisi%cn de %.1f entre %.1f es: %.1f", 162, x, y, r);
+	printf("\n La divisi%cn de %.1f entre %.1f es :  %.1f", 162, x, y, r);
 
 	_getche();
 	return 0;
