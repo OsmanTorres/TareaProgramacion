10292a02fcedcb4cf5c93f95c8158d770c927dc3
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-19 15:05:53 -0500

    Update E03-P02.cpp

--- PrimerSemestre/PrimeraParte/E03-P02.cpp
+++ PrimerSemestre/PrimeraParte/E03-P02.cpp
@@ -1,7 +1,7 @@
 /*
 Leer y desplegar una variable entera y una flotante
 Elabore un programa que defina una variable entera x, y una variable flotante y.
-Lea dos valores que le proporcione el usuario, uno entero y uno flotante).
+Lea dos valores que le proporcione el usuario, uno entero y uno flotante.
 Posteriormente despliegue los valores capturados por el usuario indicando cual es el entero y cual el flotante
 Use dos líneas de salida distintas para esto.
 */
@@ -16,8 +16,8 @@ int main() {
 
 	int x; 
 	double y;
-	printf("Introdusca un Valor Entero:  "); scanf("%d", &amp;x);
-	printf("Introdusca un Valor Flotante:  "); scanf("%f", &amp;y);
+	printf("Introdusca un Valor Entero:  "); scanf("%d", &x);
+	printf("Introdusca un Valor Flotante:  "); scanf("%f", &y);
 	printf("Valor Entero Introducido: %d", x);
 	printf("Valor Flotante Introducido: %f", y);
 	
