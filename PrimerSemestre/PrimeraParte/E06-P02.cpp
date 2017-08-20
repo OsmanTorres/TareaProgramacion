36ded68a64af583b5958d4a96839891b17790dce
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-20 13:36:23 -0500

    Create E06-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E06-P02.cpp
@@ -0,0 +1,27 @@
+/*
+Lea dos valores enteros a y b y determine si son iguales o diferentes. Muestre un mensaje en pantalla que indique el resultado
+*/
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	printf("Introduzca el Primer Numero Entero : "); scanf("%d", &a);
+	printf("Introduzca el Segundo Numero Entero : "); scanf("%d", &b);
+
+	if (a > b) {
+		printf("\nEl numero %d es mayor que %d.", a, b);
+	} else if (a < b) {
+		printf("\nEl numero %d es menor que %d.", a, b);
+	} else {
+		printf("\nEl numero %d es igual que %d.", a, b);
+	}
+
+	_getche();
+	return 0;
+}
