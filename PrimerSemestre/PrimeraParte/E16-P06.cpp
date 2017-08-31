d8bbd76b524c700d365faa1cce151a46b84afaf1
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:10:31 -0500

    Create E16-P06.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-P06.cpp
@@ -0,0 +1,13 @@
+/*
+Una empresa desea asignar cuentas de correo a todos sus empleados, para ello ha decidido seguir la siguiente filosofía:
+  a)	El correo siempre será dado en minúsculas
+  b)	La primera letra del correo será la primera letra del nombre del empleado reemplazando las “ñ” por “n” 
+  y las vocales acentuadas por vocales sin acento.
+  c)	Posteriormente se agregará el apellido paterno del empleado reemplazando las “ñ” por “n” 
+  y las vocales acentuadas por vocales sin acento.
+  d)	El correo NO puede tener espacios en blanco.
+Por ejemplo si la persona se llama “Víctor Juárez” su cuenta de correo deberá ser “vjuarez”, 
+otro ejemplo si su nombre es “Salomón Montes de Oca” su cuenta de correo deberá ser “smontesdeoca”.
+Elabore un programa que lea el nombre del empleado y su apellido paterno y regrese como resultado la cuenta de correo 
+correspondiente a dicho empleado.
+*/
