ceff06fb3edee42d12c706e76bfb46f47b40da59
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:05:13 -0500

    Create E16-P01.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-P01.cpp
@@ -0,0 +1,5 @@
+/*
+Programa que lea una palabra o frase, su programa debe validar que la palabra o frase tenga entre 5 y 20 caracteres (incluidos).
+Una vez que el valor es dado correctamente este deberá desplegar la palabra al revés. 
+Por ejemplo si la palabra dada es “perritos” su programa deberá desplegar “sotirrep”. 
+*/
