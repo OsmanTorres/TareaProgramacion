7f5fad4ce99d43e563429f0d9470eab2e9f79c58
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:51:23 -0500

    Create E12-P01.cpp

---
+++ PrimerSemestre/PrimeraParte/E12-P01.cpp
@@ -0,0 +1,12 @@
+/*
+Usando C++ y empleando SOLO variables locales construya un programa que despliegue el siguiente menú:
+  a.	Factorial
+  b.	Elevar a una potencia
+  c.	Terminar
+Su programa debe cumplir con las siguientes condiciones:
+  1)	SOLO puede usar variables locales
+  2)	El menú debe ser desplegado por una función llamada “menu()” y debe regresar la opción seleccionada.
+  3)	Debe existir la función “factorial()” la cual debe regresar el valor del factorial.
+  4)	Debe existir la función “potencia()” la cual debe regresar el valor de la potencia
+  5)	TODAS las funciones deben ser llamadas desde la función “main()”
+*/
