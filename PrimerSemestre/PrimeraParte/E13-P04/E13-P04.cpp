5d01e9afc11aecb0736c8daf70114572ddbe8018
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:36:15 -0500

    Create E13-P04.cpp

---
+++ PrimerSemestre/PrimeraParte/E13-P04/E13-P04.cpp
@@ -0,0 +1,7 @@
+/*
+Lea una lista de cinco letras, para cada letra debe validar que se trate de una letra minúscula de la “a” a la “z”,
+de lo contrario debe mandar un mensaje de error y regresar a leer la misma letra. 
+La lectura de las letras se debe hacer en una función que se llame lee_letras() y 
+el mensaje de error debe estar en una función que se llame error(). 
+Una vez que se han leído las cinco letras correctamente muestre la lista en orden alfabético.
+*/
