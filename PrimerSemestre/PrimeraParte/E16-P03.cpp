88a648ea229ebee83a699aaaa6bc765143965638
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:07:08 -0500

    Create E16-P03.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-P03.cpp
@@ -0,0 +1,23 @@
+/*
+Elabore un programa que despliegue el siguiente menú:
+  a)	Indicar la palabra
+  b)	Mostrar la palabra
+  c)	Mostrar la palabra al revés
+  d)	Mostrar vocales
+  e)	Mostrar consonantes
+  f)	Mostrar letra de una posición
+Observe que las opciones del menú están dadas con letra y el usuario podrá usar mayúsculas y minúsculas de forma indistinta 
+para seleccionar la opción deseada.
+El menú deberá estar ciclado y sólo podrá terminar cuando el usuario oprima la tecla de “ESC”
+Opción “A”: Esta opción deberá solicitar una palabra entre 5 y 20 caracteres, valide estas condiciones.
+Opción “B”: Al seleccionar esta opción se deberá  desplegar la palabra que haya sido introducida en la opción “A”.
+Opción “C”: Al seleccionar esta opción deberá desplegar la palabra de la opción “A” al revés, por ejemplo, 
+si la palabra es “teclado” su programa deberá desplegar “odalcet”.
+Opción “D”: Al seleccionar esta opción deberá desplegar las vocales contenidas en la palabra, por ejemplo, 
+si la palabra es “teclado” su programa deberá desplegar “eao”.
+Opción “E”: Al seleccionar esta opción deberá desplegar las consonantes contenidas en la palabra, por ejemplo, 
+si la palabra es “teclado” su programa deberá desplegar “tcld”.
+Opción “F”: Al seleccionar esta opción deberá solicitar un valor entero entre 0 y la longitud de la palabra (valide). 
+Cuando el valor entero es dado correctamente su programa deberá desplegar la letra correspondiente a la posición indicada. 
+Por ejemplo, si la palabra es “teclado” y el valor entero es 2, su programa deberá desplegar “c”.
+*/
