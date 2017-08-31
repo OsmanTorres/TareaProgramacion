a1a384e7c8b3daed56c7557f370cc4909fdd92fe
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:06:07 -0500

    Create E16-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-P02.cpp
@@ -0,0 +1,10 @@
+/*
+Elabore un programa que lea una palabra la cual será su cuenta de acceso, dicha cuenta deberá tener entre 5 y 15 caracteres (valide). 
+Una vez que la palabra es dada su programa debe solicitar una contraseña, dicha contraseña NO debe verse en pantalla, 
+para ello su programa deberá leer la palabra carácter por carácter e ir colocando un asterisco, 
+al final debe colocar el byte de fin de cadena “\0”. La contraseña también deberá tener entre 5 y 15 caracteres (valide). 
+Cuando ambos valores han sido dados su programa deberá validar que la cuenta sea igual a su matrícula (A0…) 
+y la contraseña deberá ser igual a su fecha de nacimiento (DDMMAA). 
+En caso de que los valores cumplan con estas condiciones su programa deberá desplegar un letrero que diga “ACCESO PERMITIDO”, 
+en caso contrario deberá desplegar un mensaje de error y regresar a solicitar la cuenta y la contraseña nuevamente
+*/
