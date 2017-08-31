5062fec6c387ba82e95b89a435812f71e6e597c3
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:56:38 -0500

    Create E17-P03.cpp

---
+++ PrimerSemestre/PrimeraParte/E17-P03.cpp
@@ -0,0 +1,6 @@
+/*
+Repita el ejercicio “1” pero ahora después de desplegar la matriz solicite un valor, posteriormente busque dicho valor en la matriz,
+si el valor NO existe en la matriz, despliegue un mensaje de error y regrese a solicitar el valor. 
+En caso de que el valor si exista despliegue las coordenadas de donde se encuentra el valor, 
+por ejemplo si nos indican el valor “88” la coordenadas son (1,2).
+*/
