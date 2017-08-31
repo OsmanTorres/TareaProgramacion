79646fd574cb11c05dc7b8433d8b86f78a302e9c
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:07:55 -0500

    Create E16-P04.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-P04.cpp
@@ -0,0 +1,11 @@
+/*
+La seguridad informática es un tema de suma importancia por lo que la construcción de una contraseña debe ser evaluada 
+para verificar su nivel de seguridad. Así si una contraseña sólo tiene:
+  a.	Sólo letras minúsculas su nivel de seguridad es “1”. 
+  b.	Si una contraseña tiene letras minúsculas y mayúsculas su nivel de seguridad es “2”. 
+  c.	Si una contraseña tiene letras minúsculas y números su nivel de seguridad es “3”. 
+  d.	Si una contraseña tiene, letras minúsculas, mayúsculas y numero su nivel de seguridad es “4”.
+  e.	Si una contraseña tiene letras minúsculas, mayúsculas, números y otros caracteres su nivel de seguridad es “5”.
+Elabore un programa que lea una contraseña carácter por carácter sin que se vean en pantalla (coloque asteriscos). 
+Debe tener entre 5 y 15 caracteres. Regrese como resultado el nivel de seguridad de dicha contraseña.
+*/
