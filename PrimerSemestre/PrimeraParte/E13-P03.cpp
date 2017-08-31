b0b44f579cbae64e91d62a75c060b89d89a040ab
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:34:44 -0500

    Create E13-P03.cpp

---
+++ PrimerSemestre/PrimeraParte/E13-P03.cpp
@@ -0,0 +1,14 @@
+/*
+Usando C++ elabore un programa que lea una lista de diez números enteros entre 1 y 1000 (valide)  y
+los almacene en un arreglo de diez posiciones. Una vez almacenados los valores su programa debe:
+  a.	Mostrar el arreglo en el orden en que fue capturado
+  b.	Ordenar el arreglo de menor a mayor y mostrarlo en forma de lista. 
+  c.	Ordenar el arreglo de mayor a menor y mostrarlo en forma de lista
+Todo el conjunto debe verse como una tabla:
+Captura			Menor a Mayor		Mayor a Menor
+12				1			12
+3				3			5
+5				4			4
+1				5			3
+4				12			1
+*/
