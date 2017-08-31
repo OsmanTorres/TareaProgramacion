19a592952fa63c9e4d95d4b2eabd9ccd6e5216a3
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:41:27 -0500

    Create E11-P01.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P01.cpp
@@ -0,0 +1,19 @@
+/*
+Elabore un programa que lea un valor entero “n”, dicho valor debe estar entre 1 y 21.
+En caso de que el valor no cumpla con las condiciones deberá enviar un mensaje de error y regresar a solicitar el valor. 
+Una vez que el valor es leído su programa deberá pintar un cuadro de nXn asteriscos iniciando por la línea superior 
+La cual deberá pintarse lentamente de izquierda a derecha, posterior deberá pintar la línea derecha 
+La cual deberá pintarse lentamente de arriba abajo, posterior deberá pintar la línea inferior en el sentido de derecha a izquierda 
+Por último deberá pintar la línea izquierda en el sentido de abajo a arriba.
+Es decir, cada línea del cuadro deberá pintarse lentamente en el sentido de las manecillas del reloj. 
+Cuando el cuadro se ha terminado de pintar deberá hacer una pausa (getche()) y borrar el cuadro lentamente en el mismo orden 
+y sentido que fue pintado.
+Su programa debe tener las siguientes funciones:
+-	Función que pide la “n”
+-	Función que despliega el mensaje de error
+-	Función que pinta el cuadro
+-	Función que borra el cuadro
+-	Función que hace el retardo
+Por ningún motivo use variables GLOBALES.
+*/
+
