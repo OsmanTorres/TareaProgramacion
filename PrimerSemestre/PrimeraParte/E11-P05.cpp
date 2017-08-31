ccae7e59a3b4034ea4a958459849dbd76ede6327
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:46:19 -0500

    Create E11-P05.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P05.cpp
@@ -0,0 +1,7 @@
+/*
+Usando C++ elabore un programa que haga que el carrito del ejercicio “3” se mueva de la posición 1,3 a la posición 60,3 
+Avanzando de uno en uno, recuerde que para hacer el efecto de animación debe pintar, retardar y borrar. 
+Para estas tres acciones debe hacer tres funciones diferentes, es decir debe existir una función “carro” que pintará el carro.
+Una función “retardo” que será el retardador y una función “borra” que será la función que borra el carrito. 
+Las tres funciones deberán estar dentro de un ciclo que corra de 1 a 60 en la función main().
+*/
