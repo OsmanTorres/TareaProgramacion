03fe66b346d6402679b161ac113965a8073ef95a
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:45:17 -0500

    Create E11-P04.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P04.cpp
@@ -0,0 +1,11 @@
+/*
+Usando C++ elabore un programa que tenga la función “carro”, dicha función deberá ser llamada desde la función main()
+Deberá pintar un carrito como el que se muestra en la figura:
+     ***
+     ***
+ ***********
+ ***********
+ ***********
+  **     **
+  **     **
+*/
