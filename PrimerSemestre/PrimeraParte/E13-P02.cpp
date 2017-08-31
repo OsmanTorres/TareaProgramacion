feeab10c02ba7229a9a2ebc5a627066f44769d3e
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:33:45 -0500

    Create E13-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E13-P02.cpp
@@ -0,0 +1,21 @@
+/*
+Usando C++ elabore un programa que lea una lista de diez números enteros entre 1 y 1000 (valide) y los almacene 
+en un arreglo de diez posiciones, posteriormente ,muestre el siguiente menú: 
+  a.	Promedio
+  b.	Número Mayor
+  c.	Número Menor
+  d.	Lista en secuencia de Captura
+  e.	Lista Inversa a la Secuencia de Captura
+  f.	Ver valor de una posición determinada
+  g.	Terminar
+La opción “a” Debe mostrar el promedio de los diez números capturados en el arreglo
+La opción “b” Debe mostrar el número mayor de la lista
+La opción “c” debe mostrar el número menor de la lista
+La opción “d” debe mostrar el arreglo en el orden en que fue capturado
+La opción “e” debe mostrar el arreglo en forma inversa al que fue capturado
+La opción “f” debe pedir un número entre 0 y 9 (valide) y mostrar el valor almacenado en la posición del valor dado,
+por ejemplo si el usuario indica un “5”, se debe mostrar el valor almacenado en la posición 5 del arreglo.
+La opción “g” debe hacer que el programa termine
+El menú debe ser mostrado desde una función que se llame menú() y cada opción debe ser ejecutada por una función diferente.
+Puede usar variables globales y locales según lo requiera.
+*/
