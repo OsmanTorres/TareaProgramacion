2f5079b3d375b830a9bac6e91c6c3185368e6a82
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:55:48 -0500

    Create E17-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E17-P02.cpp
@@ -0,0 +1,4 @@
+/*
+2)	Repita el ejercicio “1” pero ahora después de desplegar la matriz solicite un par de coordenadas (x,y) 
+y despliegue el valor de las coordenadas indicadas, por ejemplo si las coordenadas son (0,1) el valor a desplegar es: 18.
+*/
