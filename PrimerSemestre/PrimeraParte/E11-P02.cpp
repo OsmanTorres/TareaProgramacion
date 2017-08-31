3dd480dd6aae0a30c68dfe8bbc48a86e2f8d96af
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:43:07 -0500

    Create E11-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P02.cpp
@@ -0,0 +1,9 @@
+/*
+2)	Usando C++ elabore un programa que tenga la función “cara”, dicha función deberá ser llamada desde la función main().
+Deberá pintar una cara como la que se muestra en la figura:
+ *******
+*  *  *  *
+*   *    *
+*  ***   *
+ *******
+*/
