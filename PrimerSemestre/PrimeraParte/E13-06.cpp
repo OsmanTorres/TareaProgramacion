f679105bd278f215dee836dcc0c499b52669589e
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:39:33 -0500

    Create E13-06.cpp

---
+++ PrimerSemestre/PrimeraParte/E13-06.cpp
@@ -0,0 +1,33 @@
+/*
+Elabore un programa que despliegue el siguiente menú
+  a)	Capturar Números
+  b)	Mostrar valores capturados
+  c)	Media/promedio
+  d)	Mayor
+  e)	Menor
+  f)	Mediana
+  g)	Moda
+  h)	Desviación Estándar
+  i)	Menor a mayor
+  j)	Mayor a menor
+Este menú debe estar ciclado y sólo podrá terminar cuando el usuario presione la tecla de ESC (el código ASCII es 27)
+Opción “a”: Debe permitir capturar un grupo NO definido de números flotantes hasta un máximo de 100 números, 
+la captura deberá terminar cuando se completen 100 números o cuando el usuario capture un valor de CERO. 
+Dichos valores deben ser almacenados en un arreglo.
+Opción “b”: Debe mostrar la lista de los números que fueron capturados 
+Opción “c”: Debe calcular y desplegar el promedio de los “n” números capturados. 
+Opción “d”: Debe seleccionar y desplegar el  número mayor de los “n” números capturados. 
+Opción “e”: Debe seleccionar y desplegar el  número menor de los “n” números capturados. 
+Opción “f”: Debe calcular y desplegar la mediana de los “n” números capturados. OJO!! La mediana NO es el promedio.  
+Opción “g”: Debe calcular y desplegar la moda de los “n” números capturados. 
+Opción “h”: Debe calcular y desplegar la desviación estándar de los “n” números capturados. 
+Debe usar la librería <Math.h> para obtener la raíz “sqrt()”.
+Opción “i”: Debe mostrar la lista de los números ordenados de menor a mayor 
+Opción “h”: Debe mostrar la lista de los números ordenados de mayor a menor
+Las funciones que deben estar incluidas en su programa deben ser:
+-	menu()		- media()		- mayor()
+-	menor()		- moda()			- mediana()
+-	desv_est()		- muestra_nums()	- error()
+-	lista_men_may()	- lista_may_men()
+NO es forzoso usar estos nombre puede usar los que quiera pero CADA OPCION del menú debe ser una función DENTRO del mismo programa
+*/
