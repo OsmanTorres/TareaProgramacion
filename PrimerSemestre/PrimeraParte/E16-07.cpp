65d759adcdbcdb33991580584dddcf014f56d07c
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:11:51 -0500

    Create E16-07.cpp

---
+++ PrimerSemestre/PrimeraParte/E16-07.cpp
@@ -0,0 +1,13 @@
+/*
+Elabore un programa que lea dos palabras, ambas palabras deben tener entre 5 y 20 caracteres (valide). 
+Una vez leídas ambas palabras su programa deberá mostrar como salida:
+  Longitud en caracteres de la primera palabra
+  Longitud en caracteres de la segunda palabra
+  Compara ambas palabras e indicar si son iguales
+EN CASO DE QUE NO SEAN IGUALES deberá comparar carácter por carácter e indicar si son iguales en los primeros “n” caracteres, 
+por ejemplo suponga que se capturo “perro” y “perrito”. 
+En este caso ambas palabras son diferentes pero son iguales en los primeros 4 caracteres (perr).
+  Convertir las palabras a mayúsculas y desplegarlas
+  Unir ambas palabras con un espacio en blanco en el medio y decir la frase completa, 
+por ejemplo, si se captura “gato” y “bonito” su frase completa deberá decir “gato bonito”.
+*/
