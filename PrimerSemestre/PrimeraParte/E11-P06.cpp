58de5ff3f2651bafcf72e9f70722437aa1f60326
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:47:04 -0500

    Create E11-P06.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P06.cpp
@@ -0,0 +1,24 @@
+/*
+Escriba y comente el siguiente codigo:
+*/
+
+# include <stdio.h>
+int i;
+
+void uno()
+{
+	int i; i=1;	
+}
+
+void dos()
+{
+	int i; i=2;
+}
+
+void main()
+{
+	i=10;
+	uno();
+	dos();
+        printf("El valor final de I es: %d",i);
+}
