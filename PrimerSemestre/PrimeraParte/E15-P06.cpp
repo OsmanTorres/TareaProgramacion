c627c65a2ae554b8f230d5957c491f752fe8a0fa
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:30:51 -0500

    Create E15-P06.cpp

---
+++ PrimerSemestre/PrimeraParte/E15-P06.cpp
@@ -0,0 +1,10 @@
+/*
+Se requiere registrar la información de las calificaciones (0 a 100) obtenidas en el examen de programación de un grupo de estudiantes:
+  a.	Se deben capturar las calificaciones de 10 alumnos validando que se encuentren entre cero y 100.
+  b.	Después de capturar las calificaciones se debe indicar la siguiente información de salida
+  -	Promedio del grupo
+  -	Promedio de aprobados
+  -	Promedio de reprobados
+  -	Cantidad y porcentaje de aprobados
+  -	Cantidad y porcentaje de reprobados
+*/
