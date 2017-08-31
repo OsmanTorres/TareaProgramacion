30b5625f843894c7546ba8071843e3d49bd92e2a
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:57:22 -0500

    Create E17-P04.cpp

---
+++ PrimerSemestre/PrimeraParte/E17-P04.cpp
@@ -0,0 +1,16 @@
+/*
+Lea 9 valores enteros entre 1 y 999 (incluidos) en un arreglo de dos dimensiones (3X3), 
+repita la acción en un segundo arreglo de 3X3. En un tercer arreglo guarde la suma de los valores de las dos matrices anteriores. Finalmente despliegue las tres matrices. Debe verse de la siguiente forma:
+Primera Matriz
+25	21	48
+31	5	16
+10	21	57
+Segunda Matriz
+16	1	12
+30	25	61
+100	200	215
+Resultados
+41	22	60
+61	30	77
+110	221	272
+*/
