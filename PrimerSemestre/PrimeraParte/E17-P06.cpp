cc2b703a01cb3b5a035277e9292b8f012b860f0a
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 21:01:20 -0500

    Create E17-P06.cpp

---
+++ PrimerSemestre/PrimeraParte/E17-P06.cpp
@@ -0,0 +1,19 @@
+/*
+En una matriz de 3X3 coloque de forma desordenada los números del 1 al 9, su matriz debe quedar de la siguiente forma:
+3	7	1
+5	9	8
+2	4	6
+Una vez que los valores han sido capturados su programa debe pedir un par de números, ambos valores deben estar entre 1 y 9 
+y ser diferentes entre si, por ejemplo suponga que los valores dados son: 3 y 6, 
+con esta acción su programa debe intercambiar los valores en la matriz, quedando ahora de la siguiente forma:
+6       7	   1
+5	9	   8
+2       4	   3
+Observe como el 3 y el 6 han sido intercambiados, esta acción debe repetirse hasta que TODAS las columnas, 
+TODOS los renglones y AMBAS diagonales sumen 15, este es un ejemplo:
+	      		15
+    4  	3	8	15
+    9  	5	1	15
+    2  	7	6	15
+15	15	15	15
+*/
