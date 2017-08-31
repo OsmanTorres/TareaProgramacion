289f1c32da6c03c5ad8f553998cb1f7b575aeb7e
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:27:17 -0500

    Create E15-P02.cpp

---
+++ PrimerSemestre/PrimeraParte/E15-P02.cpp
@@ -0,0 +1,5 @@
+/*
+Escribir un programa que llene un arreglo de 10 números enteros aleatorios comprendidos entre 50 y 100.
+Copie en otro arreglo esos números multiplicados por 0,5 muestre ambos arreglos. 
+Investigue como funciona la instrucción “rand()” y que librería requiere.
+*/
