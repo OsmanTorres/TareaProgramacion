940427a18c37b551fb87d7d8df6033621407b9e5
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-31 10:32:47 -0500

    Create E13-P01.cpp

---
+++ PrimerSemestre/PrimeraParte/E13-P01.cpp
@@ -0,0 +1,9 @@
+/*
+Usando C++ elabore un programa que lea una lista de cinco números enteros y los almacene en un arreglo de cinco posiciones, 
+esta acción debe estar en un función que se llame captura(). Posteriormente 
+a.	Muestre el arreglo en forma de lista en el mismo orden en que los números fueron capturados y haga una pausa (getche()). 
+Esta acción debe estar en un función que se llame lista().
+b.	Repita el paso “a” pero ahora muestre los números empezando por el último número capturado y terminando por el primero, 
+es decir en el orden inverso, esta acción deberá estar en un a función que se llame inverso().
+Todas las funciones deben trabajar con variables locales, la única variable global será el propio arreglo.
+*/
