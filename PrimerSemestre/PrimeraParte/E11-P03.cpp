67ad2d9b01345d264d1333518ef005aad8d7cec6
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:44:15 -0500

    Create E11-P03.cpp

---
+++ PrimerSemestre/PrimeraParte/E11-P03.cpp
@@ -0,0 +1,9 @@
+/*
+Usando C++ elabore un programa que lea dos valores enteros entre 1 y 10 (valide estos valores).
+En caso de que los valores NO cumplan con el rango establecido su programa deberá mostrar un mensaje de error.
+Dicho mensaje de error deberá ser llamado mediante una función llamada ERROR.
+Los valores deben ser pedidos a través de una función que se llame “pide_datos”. 
+Una vez que ambos valores han cumplido con el rango establecido pinte nuevamente la carita del ejercicio “2” 
+Pero esta vez el asterisco de la esquina superior izquierda deberá estar en la posición (X,Y). 
+La carita deberá ser pintada mediante la función “cara”.
+*/
