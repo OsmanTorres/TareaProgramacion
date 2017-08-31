d745e409f19ddaecac6d32d9349e6946be27497a
Dario Arzaba <dario.arzaba@gmail.com>
2017-08-30 20:55:19 -0500

    Create E17-P01.cpp

---
+++ PrimerSemestre/PrimeraParte/E17-P01.cpp
@@ -0,0 +1,7 @@
+/*
+Lea 9 valores enteros entre 1 y 999 (incluidos) en un arreglo de dos dimensiones (3X3), 
+despliegue la matriz en pantalla dentro de un cuadro, debe verse:
+125	32	1
+18	345	21
+57	88	91
+*/
