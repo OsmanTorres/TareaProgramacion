3e01dd8fbdad89f31eb8abfe58d52e540216f1f6
Dario Arzaba <dario.arzaba@gmail.com>
2017-09-02 13:40:47 -0500

    Update E11-P02.cpp

--- PrimerSemestre/PrimeraParte/E11-P02.cpp
+++ PrimerSemestre/PrimeraParte/E11-P02.cpp
@@ -1,5 +1,5 @@
 /*
-2)	Usando C++ elabore un programa que tenga la función “cara”, dicha función deberá ser llamada desde la función main().
+Usando C++ elabore un programa que tenga la función “cara”, dicha función deberá ser llamada desde la función main().
 Deberá pintar una cara como la que se muestra en la figura:
  *******
 *  *  *  *
@@ -7,3 +7,31 @@ Deberá pintar una cara como la que se muestra en la figura:
 *  ***   *
  *******
 */
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+void cara();
+
+int main() {
+  
+	cara();
+	_getche();
+	return 0;
+
+}
+
+void cara() {
+	printf("**************\n");
+	printf("*            *\n");
+	printf("*   *    *   *\n");
+	printf("*            *\n");
+	printf("*      *     *\n");
+	printf("*  *       * *\n");
+	printf("*   *******  *\n");
+	printf("*            *\n");
+	printf("**************\n");
+}
