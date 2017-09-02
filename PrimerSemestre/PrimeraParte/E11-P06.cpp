e5758294405c6da4855e83bf27a06d84abfa16da
Dario Arzaba <dario.arzaba@gmail.com>
2017-09-02 13:48:42 -0500

    Update E11-P06.cpp

--- PrimerSemestre/PrimeraParte/E11-P06.cpp
+++ PrimerSemestre/PrimeraParte/E11-P06.cpp
@@ -2,23 +2,26 @@
 Escriba y comente el siguiente codigo:
 */
 
-# include <stdio.h>
-int i;
+# include <stdio.h>  //Importar libreria estandar de input y output
+int i;               //Crear variable de tipo int y nombre (i)
 
-void uno()
-{
-	int i; i=1;	
+//Primer funcion crea su variable local (i) y la inicia con (1)
+void uno() {
+	int i; 
+	i = 1;	
 }
 
-void dos()
-{
-	int i; i=2;
+//Segunda funcion crea su variable local (i) y la inicia con (2)
+void dos() {
+	int i; 
+	i = 2;
 }
-
-void main()
-{
-	i=10;
+//La funcion (main) inicia la variable global con (10).
+//El valor de (i) es (10) porque la funcion printf solo tiene acceso a la variable global.
+//Si la funcion printf se encontrara dentro de las funciones entonces escribiria el valor de las variables locales.
+void main() {
+	i = 10;
 	uno();
 	dos();
-        printf("El valor final de I es: %d",i);
+        printf("El valor final de I es: %d", i);
 }
