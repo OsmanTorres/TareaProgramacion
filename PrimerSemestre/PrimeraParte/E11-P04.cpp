dd47246595fbac6de75ba3a088cde38f0cb8c4b8
Dario Arzaba <dario.arzaba@gmail.com>
2017-09-02 13:42:12 -0500

    Update E11-P04.cpp

--- PrimerSemestre/PrimeraParte/E11-P04.cpp
+++ PrimerSemestre/PrimeraParte/E11-P04.cpp
@@ -9,3 +9,30 @@ Deberá pintar un carrito como el que se muestra en la figura:
   **     **
   **     **
 */
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+void carro();
+
+int main() {
+
+	carro();
+	_getche();
+	return 0;
+
+}
+
+void carro() {
+	printf("            *********            \n");
+	printf("           ********  *           \n");
+	printf("          *********   *          \n");
+	printf("    **************************   \n");
+	printf("    **************************   \n");
+	printf("    **************************   \n");
+	printf("      *****           *****      \n");
+	printf("      *****           *****      \n");
+}
