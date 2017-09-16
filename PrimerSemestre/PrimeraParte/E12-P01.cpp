688d0fbebcb33bd8c95b5385c9906f68d4f6086b
Dario Arzaba <dario.arzaba@gmail.com>
2017-09-16 14:04:03 -0500

    Update E12-P01.cpp

--- PrimerSemestre/PrimeraParte/E12-P01.cpp
+++ PrimerSemestre/PrimeraParte/E12-P01.cpp
@@ -10,3 +10,85 @@ Su programa debe cumplir con las siguientes condiciones:
   4)	Debe existir la función “potencia()” la cual debe regresar el valor de la potencia
   5)	TODAS las funciones deben ser llamadas desde la función “main()”
 */
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+
+int menu() {
+	int opcion = 0;
+	printf("\nElige una de las siguientes opciones: ");
+	printf("\n1) Calcular Factorial ");
+	printf("\n2) Calcular Potencia");
+	printf("\n3) Terminar el Programa");
+	printf("\nOpcion: "); scanf("%d", &opcion);
+	return opcion;
+}
+unsigned long long int factorial() {
+	unsigned long long int numUno;
+	int limMin = 1;
+	int limMax = 20;
+	int pausa = 2500;
+	printf("\nIntroduzca el numero entero (Entre %d y %d) : ", limMin, limMax); scanf("%llu", &numUno);
+	while (numUno >= limMax || numUno <= limMin) {
+		printf("Error. El numero no esta entre %d y %d", limMin, limMax);
+		Sleep(pausa);
+		printf("\rIntroduzca el numero entero (Entre %d y %d) : ", limMin, limMax); scanf("%llu", &numUno);
+	}
+	unsigned long long int factorial = 1;
+	for (int i = 1; i <= numUno; i++) {
+		factorial *= i;
+	}
+	return factorial;
+}
+unsigned long long int potencia() {
+	unsigned long long int numUno;
+	unsigned long long int numDos;
+	int limMin = 1;
+	int limMax = 10;
+	int limMinDos = 1;
+	int limMaxDos = 20;
+	int pausa = 2500;
+
+	printf("\nIntroduzca el numero entero (Entre %d y %d) : ", limMin, limMax); scanf("%llu", &numUno);
+	while (numUno >= limMax || numUno <= limMin) {
+		printf("Error. El numero no esta entre %d y %d", limMin, limMax);
+		Sleep(pausa);
+		printf("\rIntroduzca el numero entero (Entre %d y %d) : ", limMin, limMax); scanf("%llu", &numUno);
+	}
+	printf("Introduzca el segundo entero (Entre %d y %d) : ", limMinDos, limMaxDos); scanf("%llu", &numDos);
+	while (numDos >= limMaxDos || numDos <= limMinDos) {
+		printf("Error. El numero no esta entre %d y %d", limMinDos, limMaxDos);
+		Sleep(pausa);
+		printf("\rIntroduzca el segundo entero (Entre %d y %d) : ", limMinDos, limMaxDos); scanf("%llu", &numDos);
+	}
+
+	unsigned long long int exponente = numUno;
+	for (int i = numDos; i > 1; i--) {
+		exponente *= numUno;
+	}
+	return exponente;
+}
+
+int main() {
+	
+	int seleccion = 0;
+	while (seleccion != 3) {
+		seleccion = menu();
+		if (seleccion == 1) {
+			unsigned long long int resultadoFactorial = factorial();
+			printf("\nFactorial: %llu", resultadoFactorial); printf("\n");
+		} else if (seleccion == 2) {
+			unsigned long long int resultadoPotencia = potencia();
+			printf("\nPotencia: %llu", resultadoPotencia); printf("\n");
+		} else if (seleccion > 3 || seleccion < 1) {
+			printf("\nError. Opcion no valida.\n");
+		}
+	}
+
+	return 0;
+}
