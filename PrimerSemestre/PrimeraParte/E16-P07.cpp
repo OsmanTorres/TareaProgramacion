a5178c63b2f5a1b2c6d3c9f76320ccd57758f702
Dario Arzaba <dario.arzaba@gmail.com>
2017-09-22 21:49:31 -0500

    Update E16-P07.cpp

--- PrimerSemestre/PrimeraParte/E16-P07.cpp
+++ PrimerSemestre/PrimeraParte/E16-P07.cpp
@@ -11,3 +11,80 @@ En este caso ambas palabras son diferentes pero son iguales en los primeros 4 ca
   Unir ambas palabras con un espacio en blanco en el medio y decir la frase completa, 
 por ejemplo, si se captura “gato” y “bonito” su frase completa deberá decir “gato bonito”.
 */
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <algorithm>
+using namespace std;
+
+string capturarPalabra(int min, int max, int text) {
+	string palabra;
+	if (text == 1) {
+		printf("\n\nIntroduce la primer palabra:  \n");
+	} else if (text == 2) {
+		printf("\n\nIntroduce la segunda palabra:  \n");
+	}
+	getline(cin, palabra);
+	while ((palabra.length()) > max || (palabra.length()) < min) {
+		printf("\nError. Debe introducir entre %d y %d caracteres.", min, max);
+		printf("\n\nIntroduce la palabra:  \n"); getline(cin, palabra);
+	}
+	palabra.erase(remove_if(palabra.begin(), palabra.end(), isspace), palabra.end());
+	return palabra;
+}
+
+string comparar(string first, string second) {
+	string iguales = ""; char c;
+	if (first.front() != second.front()){
+		return iguales;
+	} else {
+		int mejor;
+		if (first.length() > second.length()) {
+			mejor = first.length();
+		} else {
+			mejor = second.length();
+		}
+		for (int i = 0; i < mejor; ++i) {
+			if (first[i] == second[i]) {
+				c = first[i];
+				iguales += c;
+			} else {
+				return iguales;
+			}
+		}
+	}
+}
+
+int main() {
+
+	printf("\nBienvenido.\n");
+	int min = 5; int max = 20;
+	string primerPalabra = capturarPalabra(min, max, 1);
+	string segundaPalabra = capturarPalabra(min, max, 2);
+	printf("\nLongitud Primera:  %d", primerPalabra.length());
+	printf("\nLongitud Segunda:  %d", segundaPalabra.length());
+	if (primerPalabra.compare(segundaPalabra) == 0) {
+		printf("\nComparacion:  Las palabras son iguales.");
+	} else {
+		printf("\nComparacion:  Las palabras son distintas.");
+		string igualesEn = comparar(primerPalabra, segundaPalabra);
+		printf("\nIguales en (%d) caracteres: (%s)", igualesEn.length(), igualesEn.c_str());
+	}
+
+	string primerMayuscula, segundaMayuscula;
+	for (int i = 0; i < primerPalabra.length(); ++i) {
+		primerMayuscula += toupper(primerPalabra[i]);
+	}
+	for (int i = 0; i < segundaPalabra.length(); ++i) {
+		segundaMayuscula += toupper(segundaPalabra[i]);
+	}
+	printf("\nPrimer palabra en mayusculas:  %s", primerMayuscula.c_str());
+	printf("\nSegunda palabra en mayusculas:  %s", segundaMayuscula.c_str());
+	string unionPalabras = primerPalabra + " " + segundaPalabra;
+	printf("\nUnion palabras:  %s", unionPalabras.c_str());
+	_getche();
+	return 0;
+}
